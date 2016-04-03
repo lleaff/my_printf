0f123969a694d8c0746c511b5f1081613399cb72
lleaff <lleaff@users.noreply.github.com>
2016-04-03 12:58:26 +0200

    >conditional '#' flag

--- src/fmt_base.c
+++ src/fmt_base.c
@@ -6,7 +6,8 @@ char *fmt_oct_int(void *np, t_fspe *fspe)
   char *str;
 
   str = format_integer_base_unsigned(np, fspe, 8, "01234567");
-  ASSIGN_AND_FREE(str, my_strcatnew("0", str));
+  if (HAS_FLAG(fspe, '#'))
+    ASSIGN_AND_FREE(str, my_strcatnew("0", str));
   return (str);
 }
 
@@ -15,7 +16,8 @@ char *fmt_hex_int(void *np, t_fspe *fspe)
   char *str;
 
   str = format_integer_base(np, fspe, 16, "0123456789abcdef");
-  ASSIGN_AND_FREE(str, my_strcatnew("0x", str));
+  if (HAS_FLAG(fspe, '#'))
+    ASSIGN_AND_FREE(str, my_strcatnew("0x", str));
   return (str);
 }
 
@@ -24,6 +26,7 @@ char *fmt_hex_int_up(void *np, t_fspe *fspe)
   char *str;
 
   str = format_integer_base(np, fspe, 16, "0123456789ABCDEF");
-  ASSIGN_AND_FREE(str, my_strcatnew("0X", str));
+  if (HAS_FLAG(fspe, '#'))
+    ASSIGN_AND_FREE(str, my_strcatnew("0X", str));
   return (str);
 }
