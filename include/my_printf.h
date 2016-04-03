6b717661c5ab6a6790e33c9398b5d10e666bf12f
lleaff <lleaff@users.noreply.github.com>
2016-04-03 04:49:55 +0200

    organize

--- include/my_printf.h
+++ include/my_printf.h
@@ -1,10 +1,11 @@
 #ifndef MY_PRINTF_H
 #define MY_PRINTF_H
 
+char *my_sprintf(const char *format, ...);
+int  my_printf(const char *format, ...);
+
 #include <stdarg.h>
 
 char *my_vsprintf(const char *format, va_list arg);
-char *my_sprintf(const char *format, ...);
-int  my_printf(const char *format, ...);
 
 #endif /* MY_PRINTF_H */
